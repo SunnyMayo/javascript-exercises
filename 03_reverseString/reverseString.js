02a629702a145054c44da35b82c661d19fba947d
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-19 15:37:43 +1100

    resolved reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const len = word.length;
+    let newWord = "";
+    for (let i = 0; i < len; i++) {
+        let x = word.charAt(word.length-i-1);
+        newWord += x;
+    };
+    return newWord;
 };
 
 // Do not edit below this line
