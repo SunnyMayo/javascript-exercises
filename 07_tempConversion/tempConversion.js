ddcdfb10885d000a938ed75514a2a250928d34d4
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-20 09:30:28 +1100

    resolved tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let temp = (fahr - 32) * 5 / 9;
+  let celsius = Math.round(temp * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let temp = (cels * 9 / 5 ) + 32;
+  let fahrenheit = Math.round(temp * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
