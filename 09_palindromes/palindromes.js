310abee0746848ff44bf0ff0b1e36e3e612fe685
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-22 13:29:09 +1100

    resolved palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    let strippedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+    
+    let reverseString = strippedString.split('').reverse().join('');
+    // compare strings
+    return strippedString === reverseString;
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
